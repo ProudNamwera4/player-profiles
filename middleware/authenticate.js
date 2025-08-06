cd9a4e9115eabdf05a2cf8ab5adf1c54d5b45a9b
ProudNamwera4 <proudnamwera4@gmail.com>
2025-08-07 00:10:46 +0200

    authentication

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,9 +1,9 @@
 //middleware/authenticate.js
 const isAuthenticated = (req, res, next) => {
-  if (req.isAuthenticated && req.isAuthenticated()) {
-    return next(); // User authenticated, proceed
+  if (!req.isAuthenticated || !req.isAuthenticated()) {
+    return res.status(401).json("You do not have access!");
   }
-  return res.status(401).json("You do not have access!"); // Otherwise block
+  next();
 };
 
 module.exports = {
